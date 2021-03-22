16bd3e6e6d2332fd4420e98f0afb0c86c68955d3
Eden <eden@bit.dev>
2021-03-22 16:58:56 +0200

    update button. update app-bar.

--- components/app-bar/app-bar.composition.tsx
+++ components/app-bar/app-bar.composition.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { AppBar } from './app-bar';
 
+export const AppBarWithLogo = () => {
+  return (
+    <div data-tesetid="app-bar">
+      <AppBar items={menuItems}>
+        <img style={{ height: '26px' }} src={logoImg}></img>
+      </AppBar>
+    </div>
+  );
+};
+
 const menuItems = [
   { label: 'Item One', action: () => console.log('Clicked: Explor new Jokes') },
   { label: 'Item Two', action: () => console.log('Clickd: Saved jokes') },
@@ -8,11 +18,3 @@ const menuItems = [
 
 const logoImg =
   'https://bad-jokes-app.firebaseapp.com/static/media/smile.46ed6de2.png';
-
-export const AppBarWithLogo = () => {
-  return (
-    <AppBar items={menuItems}>
-      <img style={{ height: '26px' }} src={logoImg}></img>
-    </AppBar>
-  );
-};
